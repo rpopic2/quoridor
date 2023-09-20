6d2fb32f7b6b5b6a34784b520c8c1bbdb7d5da00
rpopic2 <rpopic2@gmail.com>
2023-09-20 23:59:10 +0900

    Grid: refactor

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -4,6 +4,8 @@ public class Grid : MonoBehaviour
 {
     const int ROW = 9;
     const int COLUMN = 9;
+    static readonly Vector3 START1 = new(4, 0);
+    static readonly Vector3 START2 = new(4, 8);
 
     [SerializeField] Tile tilePrefab;
     [SerializeField] Transform pawnPrefab;
@@ -11,25 +13,19 @@ public class Grid : MonoBehaviour
 
     void Awake() {
         var cam = Camera.main.transform;
-        var camerZ = cam.position.z;
-        const float spacing = 1f;
+        var cameraZ = cam.position.z;
 
-        cam.position = new(4 * spacing, 4 * spacing, -10);
+        cam.position = new(4, 4, cameraZ);
 
         for (int i = 0; i < ROW; ++i) {
             for (int j = 0; j < COLUMN; ++j) {
-                var x = spacing * i;
-                var y = spacing * j;
-                var pos = new Vector3(x, y);
+                var pos = new Vector3(i, j);
                 tiles[i, j] = Instantiate(tilePrefab, pos, Quaternion.identity, transform);
             }
         }
 
-        var pawnPos = new Vector3(4 * spacing, 0 * spacing);
-        Instantiate(pawnPrefab, pawnPos, Quaternion.identity, transform);
-
-        pawnPos = new Vector3(4 * spacing, 8 * spacing);
-        Instantiate(pawnPrefab, pawnPos, Quaternion.identity, transform);
+        Instantiate(pawnPrefab, START1, Quaternion.identity, transform);
+        Instantiate(pawnPrefab, START2, Quaternion.identity, transform);
     }
 }
 
